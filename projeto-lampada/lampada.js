2c1465a71266af844e752b0c8181bccc0e2ff9a0
selenitye <luanacs@outlook.com>
2023-06-15 20:10:22 -0300

    projetofinalizado dando certo uhuuuu

--- projeto-lampada/lampada.js
+++ projeto-lampada/lampada.js
@@ -1,30 +1,29 @@
-const turnOn = document.getElementsById( 'turnOn' );
-const turnOff = document.getElementsById( 'turnOff' );
-const lamp = document.getElementById( 'lamp' );
+const turnOn = document.getElementById ( 'turnOn' );
+const turnOff = document.getElementById ( 'turnOff' );
+const lamp = document.getElementById ( 'lamp' );
 
 function isLampBroken () {
-    return lamp.src.indexOf('quebrada') > -1
+    return lamp.src.indexOf ( 'quebrada' ) > -1
 }
 
-
 function lampOn () {
-    if (!isLampBroken ()) {
-        lamp.src='./img/ligada.jpg';
+    if ( !isLampBroken () ) {
+        lamp.src = './media/ligada.jpg';
     }
 }
 
 function lampOff () {
-    if (!isLampBroken ()) {
-        lamp.src='./img/desligada.jpg';
+    if ( !isLampBroken () ) {
+        lamp.src = './media/desligada.jpg';
     }
 }
 
 function lampBroken () {
-    return src='./img/quebrada.jpg';
+    lamp.src = './media/quebrada.jpg';
 }
 
-turnOn.addEventListener('click', lampOff);
-turnOff.addEventListener('click', lampOff);
-lamp.addEventListener('mouseover', lampOn);
-lamp.addEventListener('mouseleave', lampOff);
-lamp.addEventListener('dblclick', lampBroken);
+turnOn.addEventListener ( 'click', lampOn );
+turnOff.addEventListener ( 'click', lampOff );
+lamp.addEventListener ( 'mouseover', lampOn );
+lamp.addEventListener ( 'mouseleave', lampOff );
+lamp.addEventListener ( 'dblclick', lampBroken );
